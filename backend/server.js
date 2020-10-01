63564ec3ff891d7b467a38549458cc706ffb8388
unknown <kapiljangpangi21@gmail.com>
2020-10-01 20:19:57 +0530

    with not complete backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const products = require('./data//products')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Api is running....')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+})
