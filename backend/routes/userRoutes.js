a112fbe790a413f7e1f06fad5a7c33ded9bad336
unknown <kapiljangpangi21@gmail.com>
2020-10-06 13:41:19 +0530

    FrontEnd Authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router()
 import {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,12 +6,17 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducer'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducres'
 
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 })
 const middleware = [thunk]
 
@@ -19,8 +24,11 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage }
 }
 const store = createStore(
   reducer,
